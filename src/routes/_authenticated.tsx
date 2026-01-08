c9ecffbf98987a29dd51122d1e3bc605a71c54f0
Peter Calabrese <peter@calabrese.dev>
2026-01-07 23:34:02 -0500

    update favicon and title

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -3,6 +3,15 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
   beforeLoad: async ({ context, location }) => {
+ 
+    const now = new Date();
+    const exp = new Date(context.auth.session?.expires_at * 1000);
+
+    console.log(exp);
+    if (exp < now) {
+      context.auth.signOut();
+    }
+
     if (!context.auth.session) {
       throw redirect({
         to: "/Login",
